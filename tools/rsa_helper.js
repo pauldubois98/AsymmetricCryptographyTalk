5577f10f7601a77036e77dfc525df7e621d86696
pauldubois98 <pauldubois98@gmail.com>
2022-10-01 00:40:38 +0200

    added calc sender c

--- tools/rsa_helper.js
+++ tools/rsa_helper.js
@@ -4,12 +4,20 @@ const n_input = document.getElementById("n");
 const phi_n_input = document.getElementById("phi_n");
 const e_select = document.getElementById("e");
 const d_input = document.getElementById("d");
+const n_sender_input = document.getElementById("n_sender");
+const e_sender_input = document.getElementById("e_sender");
+const m_sender_input = document.getElementById("m_sender");
+const c_sender_input = document.getElementById("c_sender");
 var p = Number(p_select.value);
 var q = Number(q_select.value);
 var n;
 var phi_n;
 var e;
 var d;
+var n_sender;
+var e_sender;
+var m_sender;
+var c_sender;
 
 function calc_n() {
   p = Number(p_select.value);
@@ -56,7 +64,20 @@ function calc_d() {
   d_input.value = d;
 }
 
+function calc_sender_c() {
+  n_sender = Number(n_sender_input.value);
+  e_sender = Number(e_sender_input.value);
+  m_sender = Number(m_sender_input.value);
+  var c = m_sender;
+  for (let i = 1; i < e_sender; i++) {
+    c = (c * m_sender) % n;
+  }
+  c_sender = c;
+  c_sender_input.value = c_sender;
+}
+
 calc_n();
 calc_phi_n();
 calc_options_e();
 calc_d();
+calc_sender_c();
